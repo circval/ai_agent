6745eb5d55fa54ee775d0ef25a26f51b51b1e64a
circval <noah.arnett1@gmail.com>
2025-11-10 11:50:01 -0800

    plaintext api fix

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-client = genai.Client(api_key="AIzaSyD8mNhTMQOaDMhD-Uvf6DwAlrruRLkMYDY")
 
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
+client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from ai-agent!")
