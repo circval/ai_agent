01f56b3315c5bed966f8f28aab1efa6cca3a8b04
circval <noah.arnett1@gmail.com>
2025-11-10 14:48:01 -0800

    added error catch and option to input prompts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,9 +10,11 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    if len(sys.argv) == 1:
+       sys.exit(1) 
     print("Hello from ai-agent!")
     response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.0-flash-001", contents=sys.argv
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
