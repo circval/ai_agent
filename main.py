32320515f1e06668a417a89e192cdaffb136b385
circval <noah.arnett1@gmail.com>
2025-11-11 08:22:47 -0800

    11/11/25 added multiple prompt option

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
-
-
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,9 +11,15 @@ client = genai.Client(api_key=api_key)
 def main():
     if len(sys.argv) == 1:
        sys.exit(1) 
+
+    user_prompt = sys.argv[1]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     print("Hello from ai-agent!")
     response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=sys.argv
+        model="gemini-2.0-flash-001",
+    contents=messages,
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
