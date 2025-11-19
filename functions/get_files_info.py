5f26e294d2b9302cf0d835c0bad036967bd94fbb
circval <noah.arnett1@gmail.com>
2025-11-19 09:24:44 -0800

    11/19/25 - added function call capabilities

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,19 @@
 import os
+from google.genai import types
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
 
 def get_files_info(working_directory, directory="."):
     active_working = os.path.abspath(working_directory)
@@ -19,3 +34,7 @@ def get_files_info(working_directory, directory="."):
         return "\n".join(directory_content)
     except Exception as e:
         return f"Error: {e}"
+    
+
+    
+
